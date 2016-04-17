a9f015023f9697093c6a65017c3985cba7dab6b7
Eddie Webb <ollitech@gmail.com>
2016-04-16 21:44:05 -0400

    little refactoring

--- index.php
+++ index.php
@@ -1,21 +1,24 @@
 <?php
 
+	$time_start = round(microtime(true) * 1000);
+
+
+
 //grab key from file, be nice and remove any silly charatcres thatv ariod eitors might put on.
 if(file_exists('api.key'))$apiKey = preg_replace('/[^A-Za-z0-9_\-]/', '',file_get_contents('api.key'));
 if(empty($apiKey)){
 	die("Create a file names api.key in this directory. You will need to obtain the key from google cloud console.");
 }
-$geoUrl = "https://maps.googleapis.com/maps/api/geocode/json?key=" . $apiKey;
-$placesUrl="https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=" . $apiKey;
-$prices = array(0=>"Free",1=>"Cheap",2=>"Moderate",3=>"Expensive",4=>"Very Expensive");
+$prices = array(0=>"Free",1=>"Cheap",2=>"Moderate",3=>"Expensive",4=>"Very Expensive",9=>"Unknown");
+logTime("keyset");
 
 //make sure our peeps, from slack are the client
-
 if(file_exists('slack.token'))$token = preg_replace('/[^A-Za-z0-9]/', '',file_get_contents('slack.token'));
 $userToken = filter_input(INPUT_GET, 'token',FILTER_SANITIZE_STRING);
-if($token  && ($token != $userToken)){
+if(! empty($token)  && ($token != $userToken)){
 	slackError("Invalid integration token");
 }
+logTime("tokenset");
 
 
 //get arguments from slack command
@@ -26,45 +29,25 @@ if (! $argumentString ){
 $arguments = explode(" ",$argumentString);
 // 0=>zip code, 1=>optional review key word
 
+logTime("argumentsset");
 
 
-//turn zipcode into geo code
-$geoUrl .= "&address=" . $arguments[0];
-$geoInfo = getResponseTo($geoUrl);
 
-
-//use geoinfo to get nearby places
-$placesUrl .= "&location=" . $geoInfo->results[0]->geometry->location->lat . "," . $geoInfo->results[0]->geometry->location->lng;
-$placesUrl .= "&radius=8000"; //5 miles
-$placesUrl .= "&opennow"; //...
-$placesInfo = array();
 //get all types suitable for happy hour
-$types = array("bar","restaurant");
-foreach($types as $type){
-	$rez=getResponseTo($placesUrl . "&type=" . $type,true);
+$placesInfo = searchForNearbyPlaces();
 
-	if(array_key_exists("results", $rez) && sizeof( $rez['results']) > 0 ){
-		$placesInfo=array_merge($placesInfo,$rez['results']);
-	}
-
-}
+logTime("allplaces");
 
 
 
+//pick the random winner from the best
+$thisPlace = pickFirstQualifyingPlace($placesInfo);
 
-// filter down by rating (not available as request filter), and randomly pick one,
-$bestPlaces = array_filter($placesInfo,"bestPlaces");
-if( ! sizeof($bestPlaces)>0 ){
-	slackError("No places near " . $arguments[0] . " match the criteria. Try a another keyword such as \"docks\",\"specials\", etc");
-}
-$thisPlaceId = array_rand($bestPlaces,1);
-$thisPlace = $bestPlaces[$thisPlaceId];
-$photoUrl = $thisPlace['icon'];
-if($thisPlace['photos'][0]['photo_reference']){
-	$photoUrl = "https://maps.googleapis.com/maps/api/place/photo?key=".$apiKey."&maxheight=150&photoreference=" . $thisPlace['photos'][0]['photo_reference'];
-	$morePhotos = $thisPlace['photos'][0]['html_attributions'][0];
-}
+logTime("thisplace");
+//set a photo to show user
+prepareFeaturePhoto($thisPlace);
 
+logTime("photoed");
 
 
 
@@ -73,8 +56,8 @@ $response='{
     "response_type": "in_channel",
     "attachments": [
         {
-            "image_url":"'. $photoUrl .'",
-            "thumb_url":"'. $photoUrl .'", 
+            "image_url":"'. $thisPlace['photo_url'] .'",
+            "thumb_url":"'. $thisPlace['photo_url'] .'", 
             "fallback": "Go to ' . $thisPlace['name'] . '",
 
             "color": "good",
@@ -130,7 +113,6 @@ function getResponseTo($url, $asArray=false){
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	$data = curl_exec($ch);
 	$info = curl_getinfo($ch);
-	curl_close($ch);
 	if( ! $data || $info['http_code'] != 200 ){
 		$err = "ERROR (http_" . $info['http_code'] . "): ";
 		if(!$data){
@@ -140,57 +122,72 @@ function getResponseTo($url, $asArray=false){
 			$err .= $data['status'] . ", " .$data['error_message'];
 		}
 		echo   $err;
+
+		curl_close($ch);
 		die();
 	}
 
+	curl_close($ch);
 	$result = json_decode( $data, $asArray);
 
 	return $result;
 }
 
-function bestPlaces(&$place){
+function pickFirstQualifyingPlace($places){
 	global $apiKey, $arguments;
 	// pubs, bars, etc, not chain restaruants
 	$acceptableTypes = array("pub","bar");
 	$forbiddenTypes = array("bakery","cafe","store");
 
+	$winner=null;
+	foreach($places as $place){
 
-	if( ! array_intersect($acceptableTypes, $place['types'])) {
-		return false; //not acceptable
-	}
-	if(  array_intersect($forbiddenTypes, $place['types'])) {
-		return false; //not acceptable
-	}
+		//skip what we can with information we know from place summary
+		//     types we want with good rating
+		if( ! array_intersect($acceptableTypes, $place['types'])) {
+			continue; //not acceptable
+		}
+		if(  array_intersect($forbiddenTypes, $place['types'])) {
+			continue; //not acceptable
+		}
+		if(! isset($place['rating']) || $place['rating'] < 3.5){
+			continue;
+		}
+		if( ! isset($place['photos'])){
+			continue;
+		}
 
-	//also unique!
-	 static $idlist = array();
-	 if ( in_array( $place['place_id'], $idlist ) ){
-	 	return false;
-	 }else{
-	 	$idlist[] = $place['place_id'];
-	 }
-	//new/unrated places, or good ones.
-	if(! isset($place['rating']) || $place['rating'] < 3.8){
-		return false;
-	}
-
-	
-	$placeDetails = getResponseTo("https://maps.googleapis.com/maps/api/place/details/json?placeid=" . $place['place_id'] . "&key=" . $apiKey,true);
-	
-	if( !isset($placeDetails['result']['reviews'])) return false;
-	$acceptable=false;
-	foreach($placeDetails['result']['reviews'] as $review){
-		$checkFor=!empty($arguments[1])?$arguments[1]:"drink";
-		$mention = stripos($review['text'],$checkFor);
-		if($mention) {
-			$acceptable=true;
-			$place = $placeDetails['result'];
-			$place['reason']=substr($review['text'], $mention - 15, 100);
+		// now get more details (reviews) and pick first match
+		if(isMatch($place)){
+			$winner=$place;
 			break;
 		}
+		
 	}
-	return $acceptable;
+	if( empty($winner)){
+		slackError("No places near " . $arguments[0] . " match the criteria. Try a another keyword such as \"docks\",\"specials\", etc");
+	}
+	return $winner;
 
+}
+
+function isMatch(&$place){
+	global $apiKey,$arguments;
+		$placeDetails = getResponseTo("https://maps.googleapis.com/maps/api/place/details/json?placeid=" . $place['place_id'] . "&key=" . $apiKey,true);
+		
+		if( !isset($placeDetails['result']['reviews'])) return false;
+		$acceptable=false;
+		foreach($placeDetails['result']['reviews'] as $review){
+			$checkFor=!empty($arguments[1])?$arguments[1]:"drink";
+			$mention = stripos($review['text'],$checkFor);
+			if($mention) {
+				$acceptable=true;
+				$place = $placeDetails['result'];
+				$place['reason']=substr($review['text'], $mention - 15, 100);
+				if(empty($place['price_level']))$place['price_level']=9;
+				return true;
+			}
+		}
 }
 
 function slackError($message){
@@ -200,5 +197,50 @@ function slackError($message){
 	die();
 }
 
+function findLatLngForZip($zipcode){
+	global $apiKey, $arguments;
+	//turn zipcode into geo code
+	$geoUrl = "https://maps.googleapis.com/maps/api/geocode/json?key=" . $apiKey;
+	$geoUrl .= "&address=" . $arguments[0];
+	$geoInfo = getResponseTo($geoUrl);
+	return $geoInfo->results[0]->geometry->location->lat . "," . $geoInfo->results[0]->geometry->location->lng;
+}
 
+function buildSearchUrl($type,$zip){
+	global $apiKey, $arguments;
+	$placesUrl="https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=" . $apiKey;
+	//use geoinfo to get nearby places
+	$placesUrl .= "&location=" . $zip;
+	$placesUrl .= "&radius=8000"; //5 miles
+	$placesUrl .= "&opennow"; //...
+	$placesUrl .= "&type=" . $type;
+	return $placesUrl;
+}
+function searchForNearbyPlaces($types = array("bar","restaurant")){
+	global $apiKey, $arguments;
+	$zip = findLatLngForZip($arguments[0]) ;
+	$placesInfo =array();
+	foreach($types as $type){
+		$rez=getResponseTo(buildSearchUrl($type, $zip) ,true);
+		if(array_key_exists("results", $rez) && sizeof( $rez['results']) > 0 ){
+			$placesInfo=array_merge($placesInfo,$rez['results']);
+		}
+	}
+	shuffle($placesInfo);
+	return $placesInfo;
+}
+
+function prepareFeaturePhoto(&$place){	
+	global $apiKey;
+	$place['photo_url'] = $place['icon'];
+	if($place['photos'][0]['photo_reference']){
+		$place['photo_url'] = "https://maps.googleapis.com/maps/api/place/photo?key=".$apiKey."&maxheight=150&photoreference=" . $place['photos'][0]['photo_reference'];
+	}
+}
+
+function logTime($step){
+	global $time_start;
+
+	error_log($step . "time:".(round(microtime(true) * 1000)-$time_start));
+}
 ?>
